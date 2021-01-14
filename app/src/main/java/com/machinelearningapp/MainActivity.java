3d2a7ea074e8a5b12df2ff2f2906b4ae75f79e2c
Paweł Kocan <pkocan@onet.pl>
2021-01-14 01:56:03 +0100

    Fixed the problem which appeared in the oldest Android system versions, created the file_paths.xml with needed coordinates, added needed changes to the activity_main.xml, AndroidManifest.xml and MainActivity, removed unnecessary comments

--- app/src/main/java/com/machinelearningapp/MainActivity.java
+++ app/src/main/java/com/machinelearningapp/MainActivity.java
@@ -5,8 +5,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,6 +18,7 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.ml.vision.FirebaseVision;
@@ -23,10 +26,26 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     TextView textView;
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -47,7 +66,19 @@ public class MainActivity extends AppCompatActivity {
         textView.setText("");
         imageView.setImageBitmap(null);
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 101);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getApplicationContext(), "com.machinelearningapp.fileprovider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, 101);
+            }
+        }
     }
 
     public void clearResults(View view) {
@@ -57,30 +88,35 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        switch (resultCode) {
-            case 0:
-                Toast.makeText(MainActivity.this, "Zdjęcie nie zostało wykonane", Toast.LENGTH_SHORT).show();
-                break;
-            case -1:
-                if (resultCode == Activity.RESULT_OK) {
-                    Bundle bundle = data.getExtras();
-                    Bitmap bitmap = (Bitmap) bundle.get("data");
-                    imageView.setImageBitmap(bitmap);
-                    FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-                    FirebaseVision firebaseVision = FirebaseVision.getInstance();
-                    FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = firebaseVision.getOnDeviceTextRecognizer();
-                    Task<FirebaseVisionText> task = firebaseVisionTextRecognizer.processImage(firebaseVisionImage);
+        try {
+            switch (resultCode) {
+                case 0:
+                    Toast.makeText(MainActivity.this, "Zdjęcie nie zostało wykonane", Toast.LENGTH_SHORT).show();
+                    break;
+                case -1:
+                    if (resultCode == Activity.RESULT_OK) {
+                            File file = new File(mCurrentPhotoPath);
+                            Bitmap bitmap = MediaStore.Images.Media
+                                    .getBitmap(getApplicationContext().getContentResolver(), Uri.fromFile(file));
+                            imageView.setImageBitmap(bitmap);
+                            FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
+                            FirebaseVision firebaseVision = FirebaseVision.getInstance();
+                            FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = firebaseVision.getOnDeviceTextRecognizer();
+                            Task<FirebaseVisionText> task = firebaseVisionTextRecognizer.processImage(firebaseVisionImage);
 
-                    task.addOnSuccessListener(firebaseVisionText -> {
-                        String s = firebaseVisionText.getText();
-                        textView.setText(s);
-                    });
+                            task.addOnSuccessListener(firebaseVisionText -> {
+                                String s = firebaseVisionText.getText();
+                                textView.setText(s);
+                            });
 
-                    task.addOnFailureListener(e -> Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show());
-                }
-                return;
-            default:
-                super.onActivityResult(requestCode, resultCode, data);
+                            task.addOnFailureListener(e -> Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show());
+                    }
+                    return;
+                default:
+                    super.onActivityResult(requestCode, resultCode, data);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
